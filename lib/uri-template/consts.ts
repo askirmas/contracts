ce822205c1e250b051f927598bf9a50b9605f98e
Andrii Kirmas <kirmas@phystech.edu>
2021-05-10 13:25:59 +0300

    Change parser to have strict property template

--- lib/uri-template/consts.ts
+++ lib/uri-template/consts.ts
@@ -6,7 +6,12 @@ const regexEscape = new RegExp(`[\\${
   .join("\\")
 }]`, "g")
 , reserved = /[/!;,:]/g
-, expParser = /\{([+#./;?&]?)([^\}]+)\}/g
+, property = /[a-zA-Z_\$][a-zA-Z_\$0-9]*/
+, modifier = /\*|:(\d+)/
+, propWithModifier = new RegExp(`${property.source}(${modifier.source})?`)
+, expParser = new RegExp(`\\{([+#./;?&]?)(${
+  propWithModifier.source
+}(,${propWithModifier.source})*)\\}`, "g")
 // TODO replace `.+` with var pattern and without `^$`
 , keyWithActionsParser = /^(.+)(\*|:(\d+))$/
 // , expSplit = /(?:(\{[+#./;?&]?[^\}]+\}))/
